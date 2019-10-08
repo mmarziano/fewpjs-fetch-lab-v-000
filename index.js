8f4986339ef7aa4f1cac1b22bb598e245ba46cc8
Monica Marziano <mmarziano1@gmail.com>
2019-10-08 12:22:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-  fetch()
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(json => console.log(json));
 }
 
 function renderBooks(json) {
